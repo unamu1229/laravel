9ce6a0349c287fb49b9d8d49ee58787db6197f86
米田宏 <yonedahiroshi1229@gmail.com>
2017-10-09 20:02:39 +0900

    Factoryメソッドで従業員クラスを生成するようにした。

--- Package/Salary/Salary/EmpMonthlyCommission.php
+++ Package/Salary/Salary/EmpMonthlyCommission.php
@@ -7,4 +7,20 @@ namespace Package\Salary\Salary;
 class EmpMonthlyCommission extends EmpMonthly
 {
     private $commissionRate;
+
+    /**
+     * @return mixed
+     */
+    public function getCommissionRate()
+    {
+        return $this->commissionRate;
+    }
+
+    /**
+     * @param mixed $commissionRate
+     */
+    public function setCommissionRate($commissionRate)
+    {
+        $this->commissionRate = $commissionRate;
+    }
 }
