326fcf91568e23eab30563bd24b308857ee0dd71
米田宏 <yonedahiroshi1229@gmail.com>
2017-10-15 23:47:11 +0900

    リファクタリングをした

--- tests/Salary/EmployeeRepositoryTest.php
+++ tests/Salary/EmployeeRepositoryTest.php
@@ -6,14 +6,17 @@ use Package\Salary\UseCase\EmpUseCase;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Package\Salary\Repository\EmployeeRepository;
+use Package\Salary\Service\Transaction;
 
 class EmployeeRepositoryTest extends TestCase
 {
 
     public function testSave()
     {
+        $transaction = new Transaction();
+        $empsData = $transaction->getTransaction('/var/www/html/laravel/tests/Salary/Transaction/AddEmp');
         $empUseCase = new EmpUseCase();
-        $employees = $empUseCase->addEmp('/var/www/html/laravel/tests/Salary/Transaction/AddEmp');
+        $employees = $empUseCase->addEmp($empsData);
         foreach ($employees as $employee) {
             $employeeRepository = $this->app->make(EmployeeRepository::class);
             $employeeRepository->save($employee);
