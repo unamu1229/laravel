ff2bf82bf8f356189587caaed4c85235bdc3a930
米田宏 <yonedahiroshi1229@gmail.com>
2017-10-15 16:14:34 +0900

    EmpUseCase にリネームして、従業員削除のユースケースを作成した。

--- tests/Salary/EmpTest.php
+++ tests/Salary/EmpTest.php
@@ -6,11 +6,11 @@ use Package\Salary\Salary\EmpHourly;
 use Package\Salary\Salary\EmpMonthly;
 use Package\Salary\Salary\EmpMonthlyCommission;
 use Package\Salary\Service\Transaction;
-use Package\Salary\UseCase\AddEmpoyee;
+use Package\Salary\UseCase\EmpUseCase;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class AddEmpTest extends TestCase
+class EmpTest extends TestCase
 {
 
     private $addEmployee;
@@ -18,7 +18,7 @@ class AddEmpTest extends TestCase
     function setUp()
     {
         parent::setUp();
-        $this->addEmployee = new AddEmpoyee();
+        $this->addEmployee = new EmpUseCase();
     }
 
 
@@ -52,15 +52,24 @@ class AddEmpTest extends TestCase
         $employees = $this->addEmployee->exec('/var/www/html/laravel/tests/Salary/Transaction/AddEmp');
         $transaction = new Transaction();
         $delEmps = $transaction->getTransaction('/var/www/html/laravel/tests/Salary/Transaction/DelEmp');
-        foreach ($delEmps as $delEmp) {
-            foreach ($employees as $key => $employee) {
-                if ($employee->getEmpId() == $delEmp[1]) {
-                    unset($employees[$key]);
-                }
-            }
-        }
+        $empUseCase = new EmpUseCase();
+        $employees = $empUseCase->delEmp($delEmps, $employees);
+
         foreach ($employees as $employee) {
             $this->assertNotEquals(1, $employee->getEmpId());
         }
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage 該当する従業員レコードがありません
+     */
+    public function testDisabledEmpIdOnDelEmp()
+    {
+        $employees = $this->addEmployee->exec('/var/www/html/laravel/tests/Salary/Transaction/AddEmp');
+        $transaction = new Transaction();
+        $delEmps = $transaction->getTransaction('/var/www/html/laravel/tests/Salary/Transaction/DelEmpDisbledEmpId');
+        $empUseCase = new EmpUseCase();
+        $empUseCase->delEmp($delEmps, $employees);
     }
 }
