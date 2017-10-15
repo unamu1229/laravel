0bcd24775fd3002634445365bb454f5388dab94a
米田宏 <yonedahiroshi1229@gmail.com>
2017-10-15 16:14:34 +0900

    EmpUseCase にリネームして、従業員削除のユースケースを作成した。

--- Package/Salary/UseCase/EmpUseCase.php
+++ Package/Salary/UseCase/EmpUseCase.php
@@ -5,7 +5,7 @@ namespace Package\Salary\UseCase;
 use Package\Salary\Service\Transaction;
 use Package\Salary\Service\Factory;
 
-class AddEmpoyee
+class EmpUseCase
 {
     public function exec($transactionPath)
     {
@@ -20,5 +20,19 @@ class AddEmpoyee
         $employees = Factory::makeEmp($empsData);
 
         return $employees;
+    }
+
+    public function delEmp($delEmps, $emps)
+    {
+        foreach ($delEmps as $delEmp) {
+            foreach ($emps as $key => $employee) {
+                if ($employee->getEmpId() == $delEmp[1]) {
+                    unset($emps[$key]);
+                    continue 2;
+                }
+            }
+            throw new \Exception('該当する従業員レコードがありません');
+        }
+        return $emps;
     }
 }
