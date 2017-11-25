489613859a560ffffd38fff87e7122c40fdfbbb1
米田宏 <yonedahiroshi1229@gmail.com>
2017-11-25 13:00:38 +0900

    営業成績に応じて給料をもらう場合の当日の給与支払いの処理を作成した。

--- tests/Salary/PaydayTest.php
+++ tests/Salary/PaydayTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 use Package\Salary\Service\Transaction;
 use Package\Salary\Repository\EmployeeRepository;
 use Package\Salary\Repository\TimeCardRepository;
+use Package\Salary\Repository\SalesReceiptRepository;
 
 class PaydayTest extends  TestCase
 {
@@ -24,12 +25,21 @@ class PaydayTest extends  TestCase
             $payDayEmps = $employeeRepository->getAllByPayday($empData[1]);
 
             foreach ($payDayEmps as $payDayEmp) {
-                $this->assertEquals('every_friday', $payDayEmp->payDay);
                 $timeCards = $timeCardRepository->getAllWhereColumnValue('empId', $payDayEmp->empId);
                 $serviceChargeDue = $employeeRepository->getArgValueById($payDayEmp->empId, 'service_charge_dues');
-                $salary = $paydayUseCase->calcHourlySalary($payDayEmp->hourlyRate, $timeCards, $serviceChargeDue);
-                $this->assertTrue(in_array($salary, [3000, 600]));
-                $this->assertEquals('Hold', $payDayEmp->paymentType()->first()->name);
+                if ($payDayEmp->payDay == 'every_friday') {
+                    $salary = $paydayUseCase->calcHourlySalary($payDayEmp->hourlyRate, $timeCards, $serviceChargeDue);
+                    $this->assertTrue(in_array($salary, [3000]));
+                    $this->assertEquals('Hold', $payDayEmp->paymentType()->first()->name);
+                    continue;
+                }
+                if ($payDayEmp->payDay == 'biweekly_friday') {
+                    $salesReceipt = $this->app->make(SalesReceiptRepository::class);
+                    $salary = $paydayUseCase->CommissionSalary($payDayEmp->commissionRate, $salesReceipt->getAllWhereColumnValue('empId', $payDayEmp->empId), $serviceChargeDue);
+                    $this->assertEquals(680000, $salary);
+                    $this->assertEquals('Hold', $payDayEmp->paymentType()->first()->name);
+                    continue;
+                }
             }
         }
     }
